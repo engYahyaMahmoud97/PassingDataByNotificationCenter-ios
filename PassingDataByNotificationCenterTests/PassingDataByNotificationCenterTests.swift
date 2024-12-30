73bcdfe01399245944045d9143e6cd8484991767
MacOS <ahmedawadallah@MacBook-Air.local>
2024-12-30 12:44:42 +0200

    upload project

---
+++ PassingDataByNotificationCenterTests/PassingDataByNotificationCenterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PassingDataByNotificationCenterTests.swift
+//  PassingDataByNotificationCenterTests
+//
+//  Created by MacOS on 30/12/2024.
+//
+
+import Testing
+@testable import PassingDataByNotificationCenter
+
+struct PassingDataByNotificationCenterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
